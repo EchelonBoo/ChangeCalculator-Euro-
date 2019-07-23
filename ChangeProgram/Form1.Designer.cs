ec9429697df6c1baf31b171b6489fbde730af5a5
Bethany Hanrahan <bethanyhan99@gmail.com>
2019-07-23 16:57:21 +0100

    Working Application

---
+++ ChangeProgram/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace ChangeProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CalcBtn = new System.Windows.Forms.Button();
+            this.Exitbtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Txt_amount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.TxtTwo = new System.Windows.Forms.TextBox();
+            this.TxtFive = new System.Windows.Forms.TextBox();
+            this.TxtTen = new System.Windows.Forms.TextBox();
+            this.TxtTwoc = new System.Windows.Forms.TextBox();
+            this.TxtOnec = new System.Windows.Forms.TextBox();
+            this.TxtTwent = new System.Windows.Forms.TextBox();
+            this.TxtFifty = new System.Windows.Forms.TextBox();
+            this.TxtOne = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CalcBtn
+            // 
+            this.CalcBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.CalcBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CalcBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CalcBtn.Location = new System.Drawing.Point(107, 517);
+            this.CalcBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CalcBtn.Name = "CalcBtn";
+            this.CalcBtn.Size = new System.Drawing.Size(75, 23);
+            this.CalcBtn.TabIndex = 0;
+            this.CalcBtn.Text = "&Calc";
+            this.CalcBtn.UseVisualStyleBackColor = false;
+            this.CalcBtn.Click += new System.EventHandler(this.CalcBtn_Click);
+            // 
+            // Exitbtn
+            // 
+            this.Exitbtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Exitbtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Exitbtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Exitbtn.Location = new System.Drawing.Point(367, 517);
+            this.Exitbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Exitbtn.Name = "Exitbtn";
+            this.Exitbtn.Size = new System.Drawing.Size(75, 23);
+            this.Exitbtn.TabIndex = 1;
+            this.Exitbtn.Text = "&Exit";
+            this.Exitbtn.UseVisualStyleBackColor = false;
+            this.Exitbtn.Click += new System.EventHandler(this.Exitbtn_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClearBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ClearBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClearBtn.Location = new System.Drawing.Point(367, 62);
+            this.ClearBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(75, 23);
+            this.ClearBtn.TabIndex = 2;
+            this.ClearBtn.Text = "C&lear";
+            this.ClearBtn.UseVisualStyleBackColor = false;
+            this.ClearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(35, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Amount";
+            // 
+            // Txt_amount
+            // 
+            this.Txt_amount.Location = new System.Drawing.Point(195, 65);
+            this.Txt_amount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Txt_amount.Name = "Txt_amount";
+            this.Txt_amount.Size = new System.Drawing.Size(100, 26);
+            this.Txt_amount.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(35, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2 euro(s)";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(35, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "One c";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(35, 380);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Two c";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(35, 331);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 20);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Five c";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label7.Location = new System.Drawing.Point(35, 288);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Ten c";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label8.Location = new System.Drawing.Point(35, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Twenty c";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label9.Location = new System.Drawing.Point(35, 210);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 20);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Fifty c";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label10.Location = new System.Drawing.Point(35, 169);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 20);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "1 euro(s)";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // TxtTwo
+            // 
+            this.TxtTwo.Location = new System.Drawing.Point(195, 126);
+            this.TxtTwo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtTwo.Name = "TxtTwo";
+            this.TxtTwo.ReadOnly = true;
+            this.TxtTwo.Size = new System.Drawing.Size(100, 26);
+            this.TxtTwo.TabIndex = 14;
+            // 
+            // TxtFive
+            // 
+            this.TxtFive.Location = new System.Drawing.Point(195, 331);
+            this.TxtFive.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtFive.Name = "TxtFive";
+            this.TxtFive.ReadOnly = true;
+            this.TxtFive.Size = new System.Drawing.Size(100, 26);
+            this.TxtFive.TabIndex = 15;
+            // 
+            // TxtTen
+            // 
+            this.TxtTen.Location = new System.Drawing.Point(195, 288);
+            this.TxtTen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtTen.Name = "TxtTen";
+            this.TxtTen.ReadOnly = true;
+            this.TxtTen.Size = new System.Drawing.Size(100, 26);
+            this.TxtTen.TabIndex = 16;
+            // 
+            // TxtTwoc
+            // 
+            this.TxtTwoc.Location = new System.Drawing.Point(195, 375);
+            this.TxtTwoc.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtTwoc.Name = "TxtTwoc";
+            this.TxtTwoc.ReadOnly = true;
+            this.TxtTwoc.Size = new System.Drawing.Size(100, 26);
+            this.TxtTwoc.TabIndex = 18;
+            // 
+            // TxtOnec
+            // 
+            this.TxtOnec.Location = new System.Drawing.Point(195, 425);
+            this.TxtOnec.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtOnec.Name = "TxtOnec";
+            this.TxtOnec.ReadOnly = true;
+            this.TxtOnec.Size = new System.Drawing.Size(100, 26);
+            this.TxtOnec.TabIndex = 19;
+            // 
+            // TxtTwent
+            // 
+            this.TxtTwent.Location = new System.Drawing.Point(195, 251);
+            this.TxtTwent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtTwent.Name = "TxtTwent";
+            this.TxtTwent.ReadOnly = true;
+            this.TxtTwent.Size = new System.Drawing.Size(100, 26);
+            this.TxtTwent.TabIndex = 21;
+            // 
+            // TxtFifty
+            // 
+            this.TxtFifty.Location = new System.Drawing.Point(195, 210);
+            this.TxtFifty.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtFifty.Name = "TxtFifty";
+            this.TxtFifty.ReadOnly = true;
+            this.TxtFifty.Size = new System.Drawing.Size(100, 26);
+            this.TxtFifty.TabIndex = 22;
+            // 
+            // TxtOne
+            // 
+            this.TxtOne.Location = new System.Drawing.Point(195, 169);
+            this.TxtOne.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.TxtOne.Name = "TxtOne";
+            this.TxtOne.ReadOnly = true;
+            this.TxtOne.Size = new System.Drawing.Size(100, 26);
+            this.TxtOne.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.CalcBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.CancelButton = this.Exitbtn;
+            this.ClientSize = new System.Drawing.Size(488, 567);
+            this.Controls.Add(this.TxtOne);
+            this.Controls.Add(this.TxtFifty);
+            this.Controls.Add(this.TxtTwent);
+            this.Controls.Add(this.TxtOnec);
+            this.Controls.Add(this.TxtTwoc);
+            this.Controls.Add(this.TxtTen);
+            this.Controls.Add(this.TxtFive);
+            this.Controls.Add(this.TxtTwo);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Txt_amount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.Exitbtn);
+            this.Controls.Add(this.CalcBtn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CalcBtn;
+        private System.Windows.Forms.Button Exitbtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Txt_amount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox TxtTwo;
+        private System.Windows.Forms.TextBox TxtFive;
+        private System.Windows.Forms.TextBox TxtTen;
+        private System.Windows.Forms.TextBox TxtTwoc;
+        private System.Windows.Forms.TextBox TxtOnec;
+        private System.Windows.Forms.TextBox TxtTwent;
+        private System.Windows.Forms.TextBox TxtFifty;
+        private System.Windows.Forms.TextBox TxtOne;
+    }
+}
+
---
+++ ChangeProgram/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangeProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exitbtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CalcBtn_Click(object sender, EventArgs e)
+        {
+            decimal twoE = (decimal)2.0;
+            decimal oneE = (decimal)1.0;
+            decimal fiftyC = (decimal)0.5;
+            decimal twentyC = (decimal)0.2;
+            decimal tenC = (decimal)0.1;
+            decimal fiveC = (decimal).05;
+            decimal twoC = (decimal).02;
+            decimal oneC = (decimal).01;
+
+            decimal money = Convert.ToDecimal(Txt_amount.Text);
+
+            int twoEAmount = (int)(money / twoE);
+            decimal change = money % twoE;
+
+            int oneEAmount = (int)(change / oneE);
+            change = change % oneE;
+
+            int fiftyCAmount = (int)(change / fiftyC);
+            change = change % fiftyC;
+
+            int twentyCAmount = (int)(change / twentyC);
+            change = change % twentyC;
+
+            int tenCAmount = (int)(change / tenC);
+            change = change % tenC;
+
+            int fiveCAmount = (int)(change / fiveC);
+            change = change % fiveC;
+
+            int twoCAmount = (int)(change / twoC);
+            change = change % twoC;
+
+            int oneCAmount = (int)(change / oneC);
+            change = change % oneC;
+
+            if (twoEAmount != 0)
+                TxtTwo.Text = Convert.ToString(twoEAmount);
+
+            if (oneEAmount != 0)
+                TxtOne.Text = Convert.ToString(oneEAmount);
+
+            if (fiftyCAmount != 0)
+                TxtFifty.Text = Convert.ToString(fiftyCAmount);
+
+            if (twentyCAmount != 0)
+                TxtTwent.Text = Convert.ToString(twentyCAmount);
+
+            if (tenCAmount != 0)
+                TxtTen.Text = Convert.ToString(tenCAmount);
+
+            if (fiveCAmount != 0)
+                TxtFive.Text = Convert.ToString(fiveCAmount);
+
+            if (twoCAmount != 0)
+                TxtTwoc.Text = Convert.ToString(twoCAmount);
+
+            if (oneCAmount != 0)
+                TxtOnec.Text = Convert.ToString(oneCAmount);
+        }
+
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            Txt_amount.Text = "";
+            TxtTwo.Text = "0";
+            TxtOne.Text = "0";
+            TxtFifty.Text = "0";
+            TxtTwent.Text = "0";
+            TxtTen.Text = "0";
+            TxtFive.Text = "0";
+            TxtTwoc.Text = "0";
+            TxtOnec.Text = "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
